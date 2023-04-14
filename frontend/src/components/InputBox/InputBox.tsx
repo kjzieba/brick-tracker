47bba909f2f871f249c50be4a706be85b82f6b22
Kacper <kacperjanzieba@gmail.com>
2023-04-14 21:31:15 +0200

    make style improvements

--- frontend/src/components/InputBox/InputBox.tsx
+++ frontend/src/components/InputBox/InputBox.tsx
@@ -2,13 +2,15 @@ import style from './InputBox.module.css';
 
 interface InputProps {
     type: string;
+    onChange?: any
 }
 
 const InputBox = (props: InputProps) => {
     return (
         <>
             <input className={style.input}
-                   type={props.type}></input>
+                   type={props.type}
+                   onChange={props.onChange}></input>
         </>
     );
 };
