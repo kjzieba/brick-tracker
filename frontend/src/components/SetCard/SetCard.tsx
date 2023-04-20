3b7c282f5374b03f09f24970787566505a61f189
Kacper <kacperjanzieba@gmail.com>
2023-04-20 16:34:01 +0200

    add custom sets page

---
+++ frontend/src/components/SetCard/SetCard.tsx
@@ -0,0 +1,47 @@
+import style from './SetCard.module.css';
+import up from '../../assets/upvote.svg'
+import down from '../../assets/downvote.svg'
+import {Link} from 'react-router-dom';
+
+interface SetCardProps {
+    id: any,
+    title: string,
+    photo: any,
+    creationDate: string
+}
+
+const SetCard = (props: SetCardProps) => {
+    return (
+        <>
+            <Link to={`/custom-sets/${props.id}`} className={style.link}>
+                <div className={style.card}>
+                    <h2 className={style.title}>{props.title}</h2>
+                    <div className={style.img_placeholder}>
+                        <img className={style.img} src={`data:image/png;base64,${props.photo}`}
+                             alt={''}></img>
+                    </div>
+                    <div className={style.votes}>
+                        <div className={style.vote}>
+                            <img src={up} alt={'Upvote'} className={style.img_vote}></img>
+                            <div className={style.number}>
+                                <b>123</b>
+                            </div>
+                        </div>
+                        <div className={style.vote}>
+                            <img src={down} alt={'Down-vote'} className={style.img_vote}></img>
+                            <div className={style.number}>
+                                <b>9</b>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={style.info}>
+                        <b>Creator</b>
+                        <b>{props.creationDate}</b>
+                    </div>
+                </div>
+            </Link>
+        </>
+    );
+};
+
+export default SetCard;
---
+++ frontend/src/pages/CustomSets/CustomSets.tsx
@@ -0,0 +1,49 @@
+import style from './CustomSets.module.css';
+import Navbar from '../../components/Navbar/Navbar';
+import Footer from '../../components/Footer/Footer';
+import SetCard from '../../components/SetCard/SetCard';
+import Button from '../../components/Button/Button';
+import {useEffect, useState} from 'react';
+
+type CustomSet = {
+    customSetId: string,
+    title: string,
+    photo: any,
+    creationDate: string
+};
+
+function CustomSets() {
+    const [customSetList, setCustomSetList] = useState<CustomSet[]>([]);
+    useEffect(() => {
+        fetch('http://127.0.0.1:8080/custom-sets', {
+                method: 'GET'
+            }
+        ).then((response) => response.json())
+            .then((data) => {
+                setCustomSetList(data);
+            });
+    }, []);
+
+    return (
+        <>
+            <div className={style.custom_sets}>
+                <Navbar/>
+                <div style={{width: 10, alignSelf: "center"}}>
+                    <Button text={"Add set"} redirection={'/add-set'}/>
+                </div>
+                {/*component search bar*/}
+                <div className={style.container}>
+                    <div className={style.grid}>
+                        {customSetList && customSetList.map(customSet => <SetCard
+                            id={customSet.customSetId} {...customSet}/>)}
+                    </div>
+                </div>
+                <div className={style.footer}>
+                    <Footer/>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default CustomSets;
